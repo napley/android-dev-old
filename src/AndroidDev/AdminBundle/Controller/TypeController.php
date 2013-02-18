9453e058a8696e7c8dffa7bb398c99660992a5f5
fabien <fabien@fabien-ubuntu>
2013-02-18 22:52:04 +0100

    Ajout gestion de type

---
+++ src/AndroidDev/AdminBundle/Controller/TypeController.php
@@ -0,0 +1,191 @@
+<?php
+
+namespace AndroidDev\AdminBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use AndroidDev\SiteBundle\Entity\Type;
+use AndroidDev\AdminBundle\Form\TypeType;
+
+/**
+ * Type controller.
+ *
+ */
+class TypeController extends Controller
+{
+    /**
+     * Lists all Type entities.
+     *
+     * @Template()
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entities = $em->getRepository('AndroidDevSiteBundle:Type')->findAll();
+
+        return array(
+            'entities' => $entities,
+        );
+    }
+
+    /**
+     * Finds and displays a Type entity.
+     *
+     * @Template()
+     */
+    public function showAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('AndroidDevSiteBundle:Type')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Type entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+
+        return array(
+            'entity'      => $entity,
+            'delete_form' => $deleteForm->createView(),
+        );
+    }
+
+    /**
+     * Displays a form to create a new Type entity.
+     *
+     * @Template()
+     */
+    public function newAction()
+    {
+        $entity = new Type();
+        $form   = $this->createForm(new TypeType(), $entity);
+
+        return array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        );
+    }
+
+    /**
+     * Creates a new Type entity.
+     *
+     * @Method("POST")
+     * @Template("AndroidDevSiteBundle:Type:new.html.twig")
+     */
+    public function createAction(Request $request)
+    {
+        $entity  = new Type();
+        $form = $this->createForm(new TypeType(), $entity);
+        $form->bind($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($entity);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('admin_type_show', array('id' => $entity->getId())));
+        }
+
+        return array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        );
+    }
+
+    /**
+     * Displays a form to edit an existing Type entity.
+     *
+     * @Template()
+     */
+    public function editAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('AndroidDevSiteBundle:Type')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Type entity.');
+        }
+
+        $editForm = $this->createForm(new TypeType(), $entity);
+        $deleteForm = $this->createDeleteForm($id);
+
+        return array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        );
+    }
+
+    /**
+     * Edits an existing Type entity.
+     *
+     * @Method("POST")
+     * @Template("AndroidDevSiteBundle:Type:edit.html.twig")
+     */
+    public function updateAction(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('AndroidDevSiteBundle:Type')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Type entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+        $editForm = $this->createForm(new TypeType(), $entity);
+        $editForm->bind($request);
+
+        if ($editForm->isValid()) {
+            $em->persist($entity);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('admin_type_edit', array('id' => $id)));
+        }
+
+        return array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        );
+    }
+
+    /**
+     * Deletes a Type entity.
+     *
+     * @Method("POST")
+     */
+    public function deleteAction(Request $request, $id)
+    {
+        $form = $this->createDeleteForm($id);
+        $form->bind($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $entity = $em->getRepository('AndroidDevSiteBundle:Type')->find($id);
+
+            if (!$entity) {
+                throw $this->createNotFoundException('Unable to find Type entity.');
+            }
+
+            $em->remove($entity);
+            $em->flush();
+        }
+
+        return $this->redirect($this->generateUrl('admin_type'));
+    }
+
+    private function createDeleteForm($id)
+    {
+        return $this->createFormBuilder(array('id' => $id))
+            ->add('id', 'hidden')
+            ->getForm()
+        ;
+    }
+}
