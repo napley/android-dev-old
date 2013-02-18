49d2f3538099b2d0324db4352b86c9d1f3b70cb7
fabien <fabien@fabien-ubuntu>
2013-02-18 23:03:52 +0100

    Ajout gestion de catégorie

---
+++ src/AndroidDev/AdminBundle/Controller/CategorieController.php
@@ -0,0 +1,191 @@
+<?php
+
+namespace AndroidDev\AdminBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use AndroidDev\SiteBundle\Entity\Categorie;
+use AndroidDev\AdminBundle\Form\CategorieType;
+
+/**
+ * Categorie controller.
+ *
+ */
+class CategorieController extends Controller
+{
+    /**
+     * Lists all Categorie entities.
+     *
+     * @Template()
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entities = $em->getRepository('AndroidDevSiteBundle:Categorie')->findAll();
+
+        return array(
+            'entities' => $entities,
+        );
+    }
+
+    /**
+     * Finds and displays a Categorie entity.
+     *
+     * @Template()
+     */
+    public function showAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('AndroidDevSiteBundle:Categorie')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Categorie entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+
+        return array(
+            'entity'      => $entity,
+            'delete_form' => $deleteForm->createView(),
+        );
+    }
+
+    /**
+     * Displays a form to create a new Categorie entity.
+     *
+     * @Template()
+     */
+    public function newAction()
+    {
+        $entity = new Categorie();
+        $form   = $this->createForm(new CategorieType(), $entity);
+
+        return array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        );
+    }
+
+    /**
+     * Creates a new Categorie entity.
+     *
+     * @Method("POST")
+     * @Template("AndroidDevSiteBundle:Categorie:new.html.twig")
+     */
+    public function createAction(Request $request)
+    {
+        $entity  = new Categorie();
+        $form = $this->createForm(new CategorieType(), $entity);
+        $form->bind($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($entity);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('admin_categorie_show', array('id' => $entity->getId())));
+        }
+
+        return array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        );
+    }
+
+    /**
+     * Displays a form to edit an existing Categorie entity.
+     *
+     * @Template()
+     */
+    public function editAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('AndroidDevSiteBundle:Categorie')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Categorie entity.');
+        }
+
+        $editForm = $this->createForm(new CategorieType(), $entity);
+        $deleteForm = $this->createDeleteForm($id);
+
+        return array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        );
+    }
+
+    /**
+     * Edits an existing Categorie entity.
+     *
+     * @Method("POST")
+     * @Template("AndroidDevSiteBundle:Categorie:edit.html.twig")
+     */
+    public function updateAction(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('AndroidDevSiteBundle:Categorie')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Categorie entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+        $editForm = $this->createForm(new CategorieType(), $entity);
+        $editForm->bind($request);
+
+        if ($editForm->isValid()) {
+            $em->persist($entity);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('admin_categorie_edit', array('id' => $id)));
+        }
+
+        return array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        );
+    }
+
+    /**
+     * Deletes a Categorie entity.
+     *
+     * @Method("POST")
+     */
+    public function deleteAction(Request $request, $id)
+    {
+        $form = $this->createDeleteForm($id);
+        $form->bind($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $entity = $em->getRepository('AndroidDevSiteBundle:Categorie')->find($id);
+
+            if (!$entity) {
+                throw $this->createNotFoundException('Unable to find Categorie entity.');
+            }
+
+            $em->remove($entity);
+            $em->flush();
+        }
+
+        return $this->redirect($this->generateUrl('admin_categorie'));
+    }
+
+    private function createDeleteForm($id)
+    {
+        return $this->createFormBuilder(array('id' => $id))
+            ->add('id', 'hidden')
+            ->getForm()
+        ;
+    }
+}
